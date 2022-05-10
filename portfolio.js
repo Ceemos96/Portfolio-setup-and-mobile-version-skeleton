0a8aae369980369e21627ff6a35a6760e2fc1fcc
ceemos <olaide.suleiman96@gmail.com>
2022-05-10 23:53:16 +0100

    Update code for toggle functionality

--- portfolio.js
+++ portfolio.js
@@ -1,11 +1,18 @@
-const bars = document.querySelector('.nav-link');
+const bars = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('#close-menu');
+const links = document.querySelector('#menu-items');
 
 bars.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
+  document.body.style.overflowY = "hidden";
 });
 
 closeMenu.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
+  document.body.style.overflowY = "scroll";
 });
+
+links.addEventListener('click', ()=>{
+    document.body.style.overflowY = "scroll";
+})
