b71bc8aae6086a3c548f0c963beef40d3ed21015
ceemos <olaide.suleiman96@gmail.com>
2022-05-10 13:43:36 +0100

    Effect corrections

--- portfolio.js
+++ portfolio.js
@@ -2,10 +2,10 @@ const bars = document.querySelector('.nav-link');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('#close-menu');
 
-bars.addEventListener('click', ()=>{
- menu.classList.toggle('show-menu')
+bars.addEventListener('click', () => {
+  menu.classList.toggle('show-menu');
 });
 
-closeMenu.addEventListener('click', ()=>{
-    menu.classList.toggle('show-menu')
-})
+closeMenu.addEventListener('click', () => {
+  menu.classList.toggle('show-menu');
+});
