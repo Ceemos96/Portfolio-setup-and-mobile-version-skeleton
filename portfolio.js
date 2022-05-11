578dd5fed40d91a865ce25062ba2ec588d955fe0
ceemos <olaide.suleiman96@gmail.com>
2022-05-11 18:05:16 +0100

    EEffect corrections

--- portfolio.js
+++ portfolio.js
@@ -1,19 +1,24 @@
 const bars = document.querySelector('.fa-bars');
 const menu = document.querySelector('.menu');
 const closeMenu = document.querySelector('#close-menu');
-const links = document.querySelector('#menu-items');
+const navLinks = document.querySelectorAll('ul > li.nav-link');
 
+// open menu when nav toggler is clicked and disabe vertical scroll
 bars.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
   document.body.style.overflowY = 'hidden';
 });
 
+// close menu when the close icon is clicked and allow vertical scroll
 closeMenu.addEventListener('click', () => {
   menu.classList.toggle('show-menu');
   document.body.style.overflowY = 'scroll';
 });
 
-links.addEventListener('click', () => {
-  document.body.style.overflowY = 'scroll';
-  menu.classList.toggle('show-menu');
-});
+// close the menu when item is clicked and allow vertical scroll
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', () => {
+    menu.classList.toggle('show-menu');
+    document.body.style.overflowY = 'scroll';
+  });
+}
